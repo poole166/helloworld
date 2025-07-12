0046e992d4482cf99e29118ee1179f5006435b9e
“poole166” <jinitaimei@giao.com>
2025-07-12 17:02:54 +0800

    6

---
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello world...");
+        return "OK";
+    }
+
+}
