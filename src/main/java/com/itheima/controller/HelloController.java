b99e46527f20d45f4dedc3b0d60206c0151f7033
“poole166” <jinitaimei@giao.com>
2025-07-12 19:17:31 +0800

    6

--- src/main/java/com/itheima/controller/HelloController.java
+++ src/main/java/com/itheima/controller/HelloController.java
@@ -12,8 +12,8 @@ public class HelloController {
 
     @GetMapping("")
     public String hello(){
-        System.out.println("hello world...");
-        return "OK";
+        System.out.println("hello world222...");
+        return "OK2";
     }
 
 }
